e4902908d71247260987ba3e686154ec6c8c2043
ShaziaAhmed2744 <you@example.com>
2022-06-19 02:27:39 -0700

    My-projects

---
+++ While Loop 26/Assets/js/app.js
@@ -0,0 +1,5 @@
+var a = 1 ;
+While(a<=10){
+    document.write("heloCity");
+    a=a+1;
+}
---
+++ chap 53 as/assests/js/app.js
@@ -0,0 +1,10 @@
+function classchange(){
+    document.getElementById("h1").className="blue"
+
+
+document.getElementById("h1").className += " fontStyle"
+// document.getElementsByTagName("h1")[0].style.fontStyle="50px"
+document.getElementsByClassName("blue")[0].getElementsByClassName.fontSize="50px"
+}
+
+
